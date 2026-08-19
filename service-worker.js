141f0908f5dcd604a8732777a2b725c902381310
bing876 <2296308012@qq.com>
2026-08-19 10:42:20 +0800

    pages: remove legacy mobile preview cache and fallback

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,8 @@
-const CACHE='anchor-mobile-preview-v10.3.1';
-const CORE=['./','./index.html','./product.html','./v10-data-p0.txt','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(c=>c.put(event.request,copy));
-    return response;
-  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
-});
+self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+  const keys=await caches.keys();
+  await Promise.all(keys.map(k=>caches.delete(k)));
+  await self.registration.unregister();
+  await self.clients.claim();
+})()));
+self.addEventListener('fetch',()=>{});
